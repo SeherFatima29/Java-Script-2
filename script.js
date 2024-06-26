d9bac0b3ab60078030b5b9b8188d479e10da3746
seherfatima <156211654+SeherFatima29@users.noreply.github.com>
2024-06-26 19:31:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var pop =+ prompt("which table you want to print !")
+var table =(pop*1)
+alert(table)
+
+var table =(pop*2)
+alert(table)
+
+var table =(pop*3)
+alert(table)
+
+var table =(pop*4)
+alert(table)
+
+var table =(pop*5)
+alert(table)
+
+var table =(pop*6)
+alert(table)
+
+var table =(pop*7)
+alert(table)
+
+var table =(pop*8)
+alert(table)
+
+var table =(pop*9)
+alert(table)
+
+var table =(pop*10)
+alert(table)
